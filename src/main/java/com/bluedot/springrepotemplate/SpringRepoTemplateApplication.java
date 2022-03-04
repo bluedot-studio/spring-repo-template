485cebdd4c9fae8dd44ecd719558cff501a388f1
Sivasankar Saravanan <siva0sankar@gmail.com>
2022-03-04 23:17:01 +0530

    Init commit

---
+++ src/main/java/com/bluedot/springrepotemplate/SpringRepoTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.bluedot.springrepotemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepoTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepoTemplateApplication.class, args);
+	}
+
+}
